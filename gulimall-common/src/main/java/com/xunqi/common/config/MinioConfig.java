11613e82a683952d77eeebb2568426c57eb67775
chengyl <chegyl@imyxg.com>
2022-05-10 13:12:34 +0800

    修改minio配置文件

--- gulimall-common/src/main/java/com/xunqi/common/config/MinioConfig.java
+++ gulimall-common/src/main/java/com/xunqi/common/config/MinioConfig.java
@@ -21,26 +21,9 @@ import org.springframework.stereotype.Component;
 @ConfigurationProperties(prefix = "minio")
 public class MinioConfig {
 
-//    @ApiModelProperty("endPoint是一个URL，域名，IPv4或者IPv6地址")
-//    private String endpoint;
-//
-//    @ApiModelProperty("TCP/IP端口号")
-//    private int port;
-//
-//    @ApiModelProperty("accessKey类似于用户ID，用于唯一标识你的账户")
-//    private String accessKey;
-//
-//    @ApiModelProperty("secretKey是你账户的密码")
-//    private String secretKey;
-//
-//    @ApiModelProperty("如果是true，则用的是https而不是http,默认值是true")
-//    private Boolean secure;
-//
+
     @ApiModelProperty("默认存储桶")
     private String bucketName="ceshi";
-//
-//    @ApiModelProperty("配置目录")
-//    private String configDir;
 
     @Bean
     public MinioClient getMinioClient() throws InvalidEndpointException, InvalidPortException {
